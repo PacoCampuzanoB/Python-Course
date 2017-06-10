b2c01d21dc308fba62add10006535aa54a55dbda
fokercito <pedronoe@comunidad.unam.mx>
2017-06-10 01:34:37 -0500

    ejercicios control de flujo

---
+++ control-de-flujo/ejercicioFor.py
@@ -0,0 +1,9 @@
+"""
+programa que imprime el conteido de una lista
+"""
+cont = 0
+nombres=['Juan','Paco','Pablo','Brayan','Kevin','Rosa']
+for n in nombres:
+	print(n)
+	cont+=1
+print('total = ',cont) 
